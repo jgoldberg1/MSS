2db5f267dca31a8949eee4b4126574e7fd185aad
Jenna Goldberg <jenmargoldberg@gmail.com>
2018-07-23 14:58:39 -0700

    Initial Commit

---
+++ MSSA Final Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSSA Final Project
+//
+//  Created by Jenna Goldberg on 7/23/18.
+//  Copyright © 2018 Jenna Goldberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
